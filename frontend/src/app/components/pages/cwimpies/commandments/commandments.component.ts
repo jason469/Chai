0bb48f18663b48bbde9eee2aa27cda3d53f7cf8a
Jason Liu <jasonliu215@gmail.com>
2022-10-26 00:16:55 +1300

    Added commandments

---
+++ frontend/src/app/components/pages/cwimpies/commandments/commandments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-commandments',
+  templateUrl: './commandments.component.html',
+  styleUrls: ['./commandments.component.css']
+})
+export class CommandmentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
