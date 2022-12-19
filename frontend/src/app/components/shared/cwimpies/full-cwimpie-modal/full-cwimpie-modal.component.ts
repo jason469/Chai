d1f78c4febf97d92f4d298fd9c1adac8e08fc950
Jason Liu <jasonliu215@gmail.com>
2022-12-20 00:39:38 +1300

    Finished modal

--- frontend/src/app/components/shared/cwimpies/full-cwimpie-modal/full-cwimpie-modal.component.ts
+++ frontend/src/app/components/shared/cwimpies/full-cwimpie-modal/full-cwimpie-modal.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnDestroy, OnInit, Renderer2, ViewChild} from '@angular/core';
 import {BsModalRef} from 'ngx-bootstrap/modal';
 import {CwimpieModalDataService} from "../../../../services/cwimpies/cwimpieModalData.service";
 import {Subscription} from "rxjs";
@@ -13,10 +13,16 @@ import {dateFormat} from "../../../../shared/constants/dateTimeFormats";
 })
 export class FullCwimpieModalComponent implements OnInit, OnDestroy {
   @Input('cwimpieData') public modalRef!: BsModalRef;
+  @ViewChild('modal') private modalContainer!: ElementRef;
+
   cwimpieModalDataServiceSubscription!: Subscription
   data!: Cwimpie;
-  birthDateString!: string;
   stampData!: IStampData
+  birthDateString!: string;
+  currentDateString!: string
+  addressNumber!: number
+  professionNames: string[] = []  // List of cwimpie's professions
+  hobbyNames: string[] = []  // List of cwimpie's hobbies
 
   constructor(
     private cwimpieModalDataService: CwimpieModalDataService,
@@ -25,7 +31,7 @@ export class FullCwimpieModalComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.cwimpieModalDataServiceSubscription = this.cwimpieModalDataService.getData().subscribe({
-      next: (data:Cwimpie) => {
+      next: (data: Cwimpie) => {
         this.data = data
         if (this.data.birthdate) {
           this.birthDateString = new Date(this.data.birthdate).toLocaleDateString('en-US', dateFormat)
@@ -39,6 +45,15 @@ export class FullCwimpieModalComponent implements OnInit, OnDestroy {
         }
       }
     });
+    this.currentDateString = new Date().toLocaleDateString('en-US', dateFormat)
+    this.addressNumber = Math.floor(Math.random() * (999 - 100 + 1) + 100)  // Random 3 digit number
+    for (let profession of this.data.professions!) {
+      this.professionNames.push(' ' + profession.name)
+    }
+
+    for (let hobby of this.data.hobbies!) {
+      this.hobbyNames.push(' ' + hobby.name)
+    }
   }
 
   ngOnDestroy(): void {
