0f7215ed57c508758cbf6ad2e342d0c3fa16aef7
jason469 <75589970+jason469@users.noreply.github.com>
2023-02-25 17:44:43 +1300

    Delete frontend/src/environments directory

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  mapbox: {
-    accessToken: 'pk.eyJ1IjoibG9nYW5sbzIxMSIsImEiOiJjbGFhem50MzQwYTI1M29udTh1amRmcDR0In0.NBnTOA_YLxVkhSSr5yBRjQ'
-  },
-  backendApiUrl: '',
-};
