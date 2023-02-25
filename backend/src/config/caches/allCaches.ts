414486136e6bd2093bb9aa582a116a969214460d
Jason Liu <jasonliu215@gmail.com>
2023-02-25 18:57:32 +1300

    Moving redis config to env file

--- backend/src/config/caches/allCaches.ts
+++ backend/src/config/caches/allCaches.ts
@@ -1,4 +1,10 @@
 import {RedisCache} from "./RedisCache";
 
-export const allCwimpiesCache = new RedisCache('', '', 'redis', 6379, 0, "cwimpie")
-export const allColoursCache = new RedisCache('', '', 'redis', 6379, 0, "colour")
+const username:string = `${process.env.REDIS_USERNAME}`
+const password:string = `${process.env.REDIS_PASSWORD}`
+const host:string = `${process.env.REDIS_HOST}`
+const port:number = Number(process.env.REDIS_PORT)
+const dbNumber:number = Number(process.env.REDIS_DBNUMBER)
+
+export const allCwimpiesCache = new RedisCache(username, password, host, port, dbNumber, "cwimpie")
+export const allColoursCache = new RedisCache(username, password, host, port, dbNumber, "colour")
