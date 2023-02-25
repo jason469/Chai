1d04d001fdb5b20e18740d6b0bf4d0062c8d5319
Jason Liu <jasonliu215@gmail.com>
2023-02-25 18:00:53 +1300

    Prep for release

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  mapbox__accessToken: 'pk.eyJ1IjoibG9nYW5sbzIxMSIsImEiOiJjbGFhem50MzQwYTI1M29udTh1amRmcDR0In0.NBnTOA_YLxVkhSSr5yBRjQ',
+  backendApiUrl: 'http://localhost:5000',
+};
